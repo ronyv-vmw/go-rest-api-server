eaa47e983e531c4df924fab18911deb467f00e7a
Rony Varghese P <ronyv@ronyv6MD6R.vmware.com>
2023-02-15 00:29:03 +0530

    learning multiple module creation

---
+++ practice/practice.go
@@ -0,0 +1,7 @@
+package practice
+
+import "fmt"
+
+func Practice() {
+	fmt.Println("Hello from practice...")
+}
---
+++ tester/tester.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/practice"
+)
+
+func main() {
+	fmt.Println("Hello world from main...!!!")
+	practice.Practice()
+}
